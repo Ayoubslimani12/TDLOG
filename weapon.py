462de13a8477c68f5cf8c446d79f88cb4c3af222
Ayoubslimani12 <114985423+Ayoubslimani12@users.noreply.github.com>
2022-11-05 11:59:31 +0100

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,12 +1,12 @@
 class weapon:
-    def __init__(self, ammunitions: int,range: int):
+    def __init__(self, ammunitions:int,range:int):
         self._ammunitions=ammunitions
         self._range=range
     def fire_at(self, x: int, y: int, z: int):
         self._x=x
         self._y=y
         self._z=z
-
+        return(x,y,z)
 class NoAmmunitionError(Exception):
 
     def _init_(self,ammunitions, message="NoAmmunitionError"):
@@ -21,7 +21,7 @@ class OutOfRangeError(Exception):
         self.message = message
         super()._init_(self.message)
 
-class Missile_antisurface(weapon):
+class Antisurface(weapon):
     def __init__(self):
         super().__init__(40,30)
 
@@ -29,13 +29,11 @@ class Missile_antisurface(weapon):
         super().fire_at(x,y,z)
         if not self._ammunitions>0:
             raise NoAmmunitionError("There is no ammunition, reload")
-        if not x**2+y**2<self._range:
-            raise OutOfRangeError(self._range)
         if not z==0:
             raise OutOfRangeError("z is not well chosen")
         self._ammunitions-=1
 
-class Missile_anti_air(weapon):
+class Anti_air(weapon):
     def __init__(self):
         super().__init__(50,40)
 
@@ -43,8 +41,6 @@ class Missile_anti_air(weapon):
         super().fire_at(x,y,z)
         if not self._ammunitions>0:
             raise NoAmmunitionError("There is no ammunition, reload")
-        if not x**2+y**2+z**2<self._range:
-            raise OutOfRangeError(self._range)
         if not z>0:
             raise OutOfRangeError("z is not well chosen")
         self._ammunitions-=1
@@ -57,9 +53,6 @@ class Torpille (weapon):
         super().fire_at(x,y,z)
         if not self._ammunitions>0:
             raise NoAmmunitionError("There is no ammunition, reload")
-        if not x**2+y**2+z**2<self._range:
-            raise OutOfRangeError(self._range)
         if not z<=0:
             raise OutOfRangeError("z is not well chosen")
         self._ammunitions-=1
-h=Torpille()
